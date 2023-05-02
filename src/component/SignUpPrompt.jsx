ce39f04520513d0f50219ede912eea089a8db4c9
PetrusAriaa <petrusariacr25@gmail.com>
2023-05-02 10:12:16 +0700

    create signup prompt component

---
+++ src/component/SignUpPrompt.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export const SignUpPrompt = ({ title, placeholder, getter }) => {
+	const [userInput, setInput] = useState('');
+	getter(userInput);
+	return (
+		<section>
+			<h2 className='text-selasar-black text-sm'>{title}</h2>
+			<input
+				className='outline-none transition-colors border-b-2 py-4 text-md focus:border-selasar-primary w-[100%] placeholder-selasar-content/30 focus:placeholder-transparent'
+				placeholder={placeholder}
+				onChange={(e) => setInput(e.target.value)}
+				value={userInput}
+				required></input>
+		</section>
+	);
+};

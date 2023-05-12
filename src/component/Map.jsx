59c415eeb55244a1c3185247fad301e2f589f52e
inamnurul <aam.inam7310@gmail.com>
2023-05-12 13:31:26 +0700

    styling map control

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,39 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet/dist/leaflet';
-import icon from '../asset/marker.svg';
+import icon1 from '../asset/marker1.svg';
+import icon2 from '../asset/marker2.svg';
 import { bingTileLayer } from '../../lib/helpers/basemaphelpers';
 function Map() {
     useEffect(() => {
-        const GSP_coordinate = [-7.770121424862446, 110.37784742786181];
+        const event1 = [-7.770121424862446, 110.37784742786181];
+        const event2 = [-7.771021424862446, 110.37684742786181];
 
-        const osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{maxZoom:18});
+        const osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { maxZoom: 18 });
         const bing = bingTileLayer('https://ecn.{s}.tiles.virtualearth.net/tiles/h{q}.jpeg?g=12825');
 
         const map = L.map('mapid', {
-            center: GSP_coordinate,
+            center: event1,
             zoom: 19,
             zoomControl: false,
             layers: [osm],
-            attributionControl:false
+            attributionControl: false
         });
-        const Icon = L.icon({
-            iconUrl:icon,
+
+
+        const Icon1 = L.icon({
+            iconUrl: icon1,
             iconSize: [70, 70]
         });
-        const marker = L.marker(GSP_coordinate, {
-            icon: Icon,
-            title: 'You',
+        const marker1 = L.marker(event1, {
+            icon: Icon1,
+            title: 'event1',
             bubblingMouseEvents: true
-        }).bindTooltip("You").openTooltip().addTo(map);
+        }).bindTooltip("event1").openTooltip().addTo(map);
 
+
+        const Icon2 = L.icon({
+            iconUrl: icon2,
+            iconSize: [70, 70]
+        });
+        const marker2 = L.marker(event2, {
+            icon: Icon2,
+            title: 'event2',
+            bubblingMouseEvents: true
+        }).bindTooltip("event2").openTooltip().addTo(map);
         return () => {
             map.remove()
         }
     }
         , [])
     return (
-        <div id='mapid' style={{ height: "100vh", width: "100vw" }}></div>
+        <div id='mapid' style={{ height: "100%", width: "100%" }}></div>
     )
 }
 export default Map;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,15 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import Map from "src/component/Map"
 import MenuButton from "src/component/MenuButton"
 import SearchButton from "src/component/SearchButton"
+import MapControl from "src/component/MapControl"
+
 function Main() {
-    return (
-      <>
-      <MenuButton/>
-      <a href="/search"><SearchButton/></a>
-      <Map/>
-      </>
-    )
-  }
-  
-  export default Main
-  
+
+  return (
+    <>
+      <div className='h-screen'>
+        <div className="h-full">
+          <MenuButton />
+          <a href="/search"><SearchButton /></a>
+          <MapControl />
+          <Map />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
